0467fd4b39ac60c4a36cd9275751beb8aedd618f
Wame Gassama <161361272+WameGassama@users.noreply.github.com>
2024-05-14 22:51:05 +0200

    Added first test

---
+++ src/app/__test__/home.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import ListItem from '../components/listItem';
+
+describe('ListItem', () => {
+  it('should display a YouTube channel', () => {
+    const { getByTestId } = render(
+      <ListItem
+        initials={'John Doe'}
+        display_name={'John Doe'}
+        handle={'@JohnDoe'}
+        subscribers={1788675}
+        uploads={909}
+        views={92897891}
+        color={''}
+      />
+    );
+
+    const initials = getByTestId('initials').textContent;
+    const displayName = getByTestId('display name').textContent;
+    const handle = getByTestId('handle').textContent;
+    const subscribers = getByTestId('subscribers').textContent;
+    const uploads = getByTestId('uploads').textContent;
+    const views = getByTestId('views').textContent;
+
+    expect(initials).toBe('JD');
+    expect(displayName).toBe('John Doe');
+    expect(handle).toBe('@JohnDoe');
+    expect(subscribers).toBe('1.8 M');
+    expect(uploads).toBe('909');
+    expect(views).toBe('92.9 M');
+  });
+});
